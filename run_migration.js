2ea71b0239c97a160d2ec7f0c63617d8b94882da
kangdaqiq <kangdaqiq@gmail.com>
2026-01-21 16:12:40 +0700

    Fix: Correct path to database config in run_migration.js

--- run_migration.js
+++ run_migration.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const db = require('../src/config/database');
+const db = require('./src/config/database');
 
 async function runMigration() {
     try {
